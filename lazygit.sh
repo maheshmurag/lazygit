7dea2dcca483be45bcb856bf2ea955460584c205
Mahesh Murag <maheshmmurag@gmail.com>
2015-12-31 14:19:12 -0800

    updated script

--- lazygit.sh
+++ lazygit.sh
@@ -1,11 +1,5 @@
 function g() {
-    addOut=$(git add .);
-    if [[ "$addOut" == *"fatal"* ]]
-    then
-        commitOut=$(git commit -a -m "$*")
-        if [[ "$commitOut" == *"fatal"* ]]
-        then
-            pushOut=$(git push)
-        fi
-    fi
+    git add .
+    git commit -a -m "$*"
+    git push
 }
